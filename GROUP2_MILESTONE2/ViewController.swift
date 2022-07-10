870dd809d39553776c5362263b6598476a06ba32
Oreoluwatemi <temitayolawal35@gmail.com>
2022-07-10 00:39:10 -0400

    Initial Commit

---
+++ GROUP2_MILESTONE2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GROUP2_MILESTONE2
+//
+//  Created by Oreoluwa Lawal on 2022-07-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
